6401d1a5ca204a86208532c0f7124face88f2fdf
samureira <samureira@hotmail.com>
2019-07-03 23:38:25 +0100

    exercises 1 and 2

---
+++ AED/EX01/EX01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("introduza um primeiro numero: ");
+            int iPrimeiroNumero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("introduza um segundo numero: ");
+            int iSegundoNumero = Int32.Parse(Console.ReadLine());
+
+            int iSoma = iPrimeiroNumero + iSegundoNumero;
+            Console.WriteLine("Resultado da soma: " + iSoma);
+
+            if (iSoma > 10)
+            {
+                Console.WriteLine("o resultado é superior a 10!");
+            }
+
+        }
+    }
+}
---
+++ AED/EX02/EX02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("type your first number: ");
+            int iFirstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("type your second number: ");
+            int iSecondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("numbers: " + iFirstNumber + " | " + iSecondNumber);
+
+            int iTotal = iFirstNumber + iSecondNumber;
+
+            if (iTotal > 10)
+            {
+                Console.WriteLine("result bigger than 10!");
+            }
+            else if (iTotal == 10)
+            {
+                Console.WriteLine("result equal to 10!");
+            }
+            else
+            {
+                Console.WriteLine("result smaller than 10!");
+            }
+  
+        }
+    }
+}
