e046365ab472fac692db17cd7a22c7c1f5eeb417
samureira <samureira@hotmail.com>
2019-07-15 22:13:51 +0100

    dev 009

---
+++ AED/EX09/EX09/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce your height: ");
+            double iHeight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce your genre: ");
+            string sGenre = Console.ReadLine();
+
+            Console.WriteLine("Given height: " + iHeight);
+            Console.WriteLine("Given genre (M/F): " + sGenre);
+
+            double iWeight;
+
+            if (sGenre == "M")
+            {
+                iWeight = (72.7 * iHeight) - 58;
+                Console.WriteLine("You should have a weight of: " + iWeight + "kgs");
+            }
+            else if (sGenre == "F")
+            {
+                iWeight = (62.1 * iHeight) - 44.7;
+                Console.WriteLine("you should have a weight of: " + iWeight + "kgs");
+            }
+            else
+            {
+                Console.WriteLine("Please introduce a proper genre value");
+            }
+        }
+    }
+}
