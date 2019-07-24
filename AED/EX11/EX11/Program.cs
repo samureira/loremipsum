6339830d3637d57c70ce3d96368f8e4b22267c91
samureira <samureira@hotmail.com>
2019-07-24 23:30:37 +0100

    eleven

---
+++ AED/EX11/EX11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of X: ");
+            double iValueX = double.Parse(Console.ReadLine());
+
+            double i1stFunc = ((5*iValueX)+3);
+            double i2ndFunc = ( ((iValueX*iValueX)-16) * 0.5);
+
+            double iFinalFunc = (i1stFunc / i2ndFunc);
+
+            Console.WriteLine("iFinalFunc: " + iFinalFunc);
+        }
+    }
+}
