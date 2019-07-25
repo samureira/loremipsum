bb2406e03e8a461e5d8bba9e025b9bf4f1d876f6
samureira <samureira@hotmail.com>
2019-07-25 22:32:44 +0100

    twelve (12) and thirteen (13) » done

---
+++ AED/EX12/EX12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double iRendimento;
+
+            Console.WriteLine("introduzir aplicacao mensal: ");
+            double iValorAplicacaoMensal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduzir taxa: ");
+            double iTaxa = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduzir numero de meses: ");
+            int iNumeroDeMeses = int.Parse(Console.ReadLine());
+
+            iRendimento = iValorAplicacaoMensal * Math.Pow((1 + iTaxa), Convert.ToDouble(iNumeroDeMeses)) - 1 / iTaxa;
+            Console.WriteLine("iRendimento: " + iRendimento);
+
+        }
+    }
+}
---
+++ AED/EX13/EX13/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int iResult = 0;
+
+            Console.WriteLine("pick a first number: ");
+            double iFirstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("pick a second number: ");
+            double iSecondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("choose the operation: ");
+
+            string sAdd = "1";
+            string sSubtract = "2";
+            string sMultiplication = "3";
+            string sDivision = "4";
+
+            string sOperation = Console.ReadLine();
+
+            if (sOperation == sAdd)
+            {
+                iResult = Convert.ToInt32(iFirstNumber) + Convert.ToInt32(iSecondNumber);
+                Console.WriteLine("(+) Result: " + iResult);
+            }
+            else if (sOperation == sSubtract)
+            {
+                iResult = Convert.ToInt32(iFirstNumber) - Convert.ToInt32(iSecondNumber);
+                Console.WriteLine("(-) Result: " + iResult);
+            }
+            else if (sOperation == sMultiplication)
+            {
+                iResult = Convert.ToInt32(iFirstNumber) * Convert.ToInt32(iSecondNumber);
+                Console.WriteLine("(*) Result: " + iResult);
+            }
+            else if(sOperation == sDivision)
+            {
+                iResult = Convert.ToInt32(iFirstNumber) / Convert.ToInt32(iSecondNumber);
+                Console.WriteLine("(/) Result: " + iResult);
+            }
+            else
+            {
+                Console.WriteLine("operation not recognized!");
+            }
+
+        }
+    }
+}
