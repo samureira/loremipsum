6559cf7e44f1ce2f97d2a1a63bd921313eb498fa
samureira <samureira@hotmail.com>
2019-08-11 20:41:22 +0100

    18 & 19

---
+++ AED/EX17/EX17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX17
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number of your choice: ");
+            int iNumber = int.Parse(Console.ReadLine());
+            int iNewNumber = iNumber * 3;
+
+            while (iNumber != -999)
+            {
+                Console.WriteLine("The triple of " + iNumber + " is: " + iNewNumber);
+                Console.WriteLine("Enter again a number of your choice: ");
+                iNumber = int.Parse(Console.ReadLine());
+                iNewNumber = iNumber * 3;
+            }
+
+            Console.WriteLine("You introduced number -999! The program is terminated!");
+        }
+    }
+}
---
+++ AED/EX18/EX18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iTotalOfNumbers = 0;
+
+            Console.WriteLine("Enter a positive number");
+            int iPosNumber = int.Parse(Console.ReadLine());
+            
+
+            while (iPosNumber > 0)
+            {
+                Console.WriteLine("Enter a positive number again");
+                iPosNumber = int.Parse(Console.ReadLine());
+                iTotalOfNumbers = iTotalOfNumbers + 1;
+            }
+
+            Console.WriteLine("Amount of numbers given: " + iTotalOfNumbers);
+        }
+    }
+}
