419c2897b5b3c669a5ebb47c80af0b4b28052509
samureira <samureira@hotmail.com>
2019-08-15 22:24:56 +0100

    ex 20 yet to be started

---
+++ LP1/EX19/EX19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numbersAbove100And200 = 0;
+            int number;
+
+            do
+            {
+                Console.WriteLine("enter a number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine("you entered the number " + number);
+
+                if (number > 100 && number < 200)
+                {
+                    numbersAbove100And200 = numbersAbove100And200 + 1;
+                }
+                else
+                {
+                    numbersAbove100And200 = numbersAbove100And200;
+                }
+
+            } while (number != 0);
+
+            Console.WriteLine(number + " was entered, the program gets terminated!");
+            Console.WriteLine("you entered " + numbersAbove100And200 + " times a number between 100 and 200");
+
+        }
+    }
+}
---
+++ LP1/EX20/EX20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+
+            do
+            {
+                Console.WriteLine("Enter a number above 0 ( or press 0 to Exit )");
+            } while ();
+        }
+    }
+}
