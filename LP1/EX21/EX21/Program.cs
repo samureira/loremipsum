1b7b0dbf4395f11885bfe052c9ca2f46062270bf
samureira <samureira@hotmail.com>
2019-08-26 22:16:10 +0100

    22 in progress

---
+++ LP1/EX21/EX21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberEntered;
+            int PowNumber;
+
+            do
+            {
+                Console.WriteLine("Enter a number: ");
+                NumberEntered = int.Parse(Console.ReadLine());
+
+                if (NumberEntered % 6 == 0)
+                {
+                    break;
+                }
+
+                PowNumber = NumberEntered * NumberEntered;
+                Console.WriteLine("the power of the number " + NumberEntered + " is " + PowNumber);
+
+            } while (NumberEntered % 6 != 0);
+
+            Console.WriteLine("Program Terminated!");
+        }
+    }
+}
---
+++ LP1/EX22/EX22/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int GivenNumber;
+            int NumbersQuantity = 0;
+            //int FlagVar = 0;
+
+            do
+            {
+                Console.WriteLine("Give a number: ");
+                GivenNumber = int.Parse(Console.ReadLine());
+
+                if (GivenNumber <= 1)
+                {
+                    Console.WriteLine(GivenNumber + "is not a prime!");
+                }
+
+                for (var i = 2; i <= GivenNumber/2; i++)
+                {
+                    if (GivenNumber % i == 0)
+                    {
+                        Console.WriteLine(GivenNumber + "is not a prime number!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(GivenNumber + " it is a prime number!");
+                        NumbersQuantity = NumbersQuantity + 1;
+                    }
+                }
+
+            } while (GivenNumber > 0);
+
+            Console.WriteLine(NumbersQuantity + " numbers were entered!");
+        }
+    }
+}
