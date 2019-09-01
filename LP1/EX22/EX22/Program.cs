a5559466bd1b9a9f830bbe07ab57376001ffcc2f
samureira <samureira@hotmail.com>
2019-09-01 17:01:29 +0100

    Ex 22 to be continued...

--- LP1/EX22/EX22/Program.cs
+++ LP1/EX22/EX22/Program.cs
@@ -13,34 +13,39 @@ namespace EX22
 
             int GivenNumber;
             int NumbersQuantity = 0;
-            //int FlagVar = 0;
+            int Count = 0;
 
             do
             {
                 Console.WriteLine("Give a number: ");
                 GivenNumber = int.Parse(Console.ReadLine());
 
-                if (GivenNumber <= 1)
-                {
-                    Console.WriteLine(GivenNumber + "is not a prime!");
-                }
+                //if (GivenNumber <= 1)
+                //{
+                //    Console.WriteLine(GivenNumber + " is not a prime!");
+                //}
 
-                for (var i = 2; i <= GivenNumber/2; i++)
+                for (var i = 1; i <= GivenNumber; i++)
                 {
                     if (GivenNumber % i == 0)
                     {
-                        Console.WriteLine(GivenNumber + "is not a prime number!");
+                        Count = Count + 1;
                     }
-                    else
-                    {
-                        Console.WriteLine(GivenNumber + " it is a prime number!");
-                        NumbersQuantity = NumbersQuantity + 1;
-                    }
+                }
+
+                if (Count == 2)
+                {
+                    Console.WriteLine(GivenNumber + " is a prime number!");
+                    NumbersQuantity = NumbersQuantity + 1;
+                }
+                else
+                {
+                    Console.WriteLine(GivenNumber + " is not a prime number!");
                 }
 
             } while (GivenNumber > 0);
 
-            Console.WriteLine(NumbersQuantity + " numbers were entered!");
+            Console.WriteLine(NumbersQuantity + " prime numbers were entered!");
         }
     }
 }
