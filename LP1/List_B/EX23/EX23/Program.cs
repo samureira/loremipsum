fd9c3e3af4b335f2a7979a147230f9b24f540ba7
samureira <samureira@hotmail.com>
2019-09-09 16:06:22 +0100

    folders and files organization

---
+++ LP1/List_B/EX23/EX23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int AccountNumber;
+            int AccountBalance;
+
+            int NegAccounts = 0;
+            int AccountsQuantity = 0;
+
+            int NegPercentage;
+
+            do
+            {
+                Console.WriteLine("Enter account number");
+                AccountNumber = int.Parse(Console.ReadLine());
+
+                if (AccountNumber <= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter account balance");
+                    AccountBalance = int.Parse(Console.ReadLine());
+
+                    if (AccountBalance < 0)
+                    {
+                        Console.WriteLine("Negative Balance!");
+                        NegAccounts = NegAccounts + 1;
+                        AccountsQuantity = AccountsQuantity + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Non-Negative Balance!");
+                        AccountsQuantity = AccountsQuantity + 1;
+                    }
+                }
+
+            } while (AccountNumber > 0);
+
+            NegPercentage = (NegAccounts / AccountsQuantity) * 100;
+            Console.WriteLine("(-) Percentage: " + NegPercentage);
+        }
+    }
+}
--- LP1/readme.txt
+++ LP1/readme.txt
@@ -1,1 +1,4 @@
 Note: for all these exercises, ignore input validation unless otherwise directed. Assume the user enters a value in the format that the program expects. For example, if the program expects the user to enter a number, don't worry about validating if the input is a number or not. When testing your program, simply enter a number.
+
+1. Sequential And Conditional Algorithms Structures [ List A ]
+2. Repetitive Algorithmic Structures [ List B ]
