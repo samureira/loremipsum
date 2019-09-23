3c66be1d959f80dcbbbccea144ebbf1af9b34d1e
samureira <samureira@hotmail.com>
2019-09-23 19:15:30 +0100

    exercise 24 done

---
+++ LP1/List_B/EX24/EX24/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int GivenNumber;
+            int TotalOfMultiples = 0;
+            int SumOfMultiples = 0;
+            int GivenNumbersAvg = 0;
+            
+            do
+            {
+                Console.WriteLine("Enter a number bigger than 0!");
+                GivenNumber = int.Parse(Console.ReadLine());
+
+                if (GivenNumber % 3 == 0)
+                {
+                    if (GivenNumber != 0)
+                    {
+                        TotalOfMultiples = TotalOfMultiples + 1;
+                        SumOfMultiples = SumOfMultiples + GivenNumber;
+                        Console.WriteLine("the number " + GivenNumber + " is a multiple of 3!");
+                    }                  
+                }
+            }
+            while (GivenNumber > 0);
+
+            GivenNumbersAvg = (SumOfMultiples / TotalOfMultiples);
+            Console.WriteLine("The average of numbers that are multiple of three is: " + GivenNumbersAvg);
+
+        }
+    }
+}
+
+// programa tem de aceitar varios numeros inteiros (se for zero, sai)
+// apresentar a media dos numeros multiplos de 3
